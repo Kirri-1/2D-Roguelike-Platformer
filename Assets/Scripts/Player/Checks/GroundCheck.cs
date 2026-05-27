479b813f8efd8a214322343d00709c479e3a4c65
Kirrito1 <kirridesu@gmail.com>
2026-05-27 09:30:22 -0500

    Updated GroundCheck
    Added more LayerMasks for future expansions. Changed Gizmos to be able to reflect the color of what you landed on (although this only affects the devs)

--- Assets/Scripts/Player/Checks/GroundCheck.cs
+++ Assets/Scripts/Player/Checks/GroundCheck.cs
@@ -1,16 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundCheck : MonoBehaviour
 {
     public Transform groundCheck;
     public LayerMask groundMask;
-    public bool isGrounded => Physics2D.OverlapCircle(groundCheck.position, 0.3f, groundMask);
+    public bool isGrounded => CheckOverlap(groundMask);
 
+    public LayerMask rechargableMask;
+    public bool isRechargable => CheckOverlap(rechargableMask);
+
+    public LayerMask drainChargeMask;
+    public bool isDrainingCharge => CheckOverlap(drainChargeMask);
+    List<(LayerMask mask, Color color, int priority)> maskColors = new();
+
+    private void Awake()
+    {
+        maskColors.Add((groundMask, Color.green, 0));
+        maskColors.Add((rechargableMask, Color.blue, 2));
+        maskColors.Add((drainChargeMask, Color.yellow, 1));
+        maskColors.Sort((a, b) => a.priority.CompareTo(b.priority));
+    }
     private void OnDrawGizmos()
     {
         if (!DebugMode.DebugModeActive)
             return;
+        foreach (var kvp in maskColors)
+        {
+            if (Physics2D.OverlapCircle(groundCheck.position, 0.3f, kvp.mask))
+            {
+                Gizmos.color = kvp.color;
+                Gizmos.DrawWireSphere(groundCheck.position, 0.3f);
+                return;
+            }
+        }
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, 0.3f);
+    }
+
+    bool CheckOverlap(LayerMask mask)
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.3f, mask);
     }
 }
